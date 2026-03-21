fd4a9331781892d3d9ac43179153830b9f834f58
Jack Hagen <jjhags@gmail.com>
2026-03-20 22:46:36 -0500

    Changed to port 5000

--- main.py
+++ main.py
@@ -353,4 +353,4 @@ def pictures():
 
 if __name__ == '__main__':
     #app.run(host="0.0.0.0", debug=True)
-    app.run(host="0.0.0.0", debug=False, port=5001)
+    app.run(host="0.0.0.0", debug=False, port=5000)
